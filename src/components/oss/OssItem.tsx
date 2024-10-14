39beee15ca531ace2f54e54acb11948f2b6e6bec
yiohsooil <yioh@sooil.com>
2024-10-14 12:50:26 +0900

    OssItem 추가

---
+++ src/components/oss/OssItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Styled } from '../../styles/oss';
+
+interface OssItemProps {
+  label: string;
+  link: string;
+  subLink?: string;
+  descriptions: string[];
+}
+
+const OssItem = ({ label, link, subLink, descriptions }: OssItemProps) => {
+  return (
+    <Styled.ListItem>
+      <p>
+        <strong>{label}</strong>
+      </p>
+      <Styled.Link href={link} target="_blank">
+        {link}
+      </Styled.Link>
+      {descriptions.map((description, index) => (
+        <p>
+          {description}{' '}
+          {index === 0 && subLink && (
+            <Styled.Link href={subLink} target="_blank">
+              {subLink}
+            </Styled.Link>
+          )}
+        </p>
+      ))}
+    </Styled.ListItem>
+  );
+};
+
+export default OssItem;
--- src/pages/Oss.tsx
+++ src/pages/Oss.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Styled } from '../styles/oss';
+import OssItem from '../components/oss/OssItem';
 
 const Oss = () => {
   return (
@@ -11,149 +12,73 @@ const Oss = () => {
         that may be contained in the application.
       </p>
       <Styled.ListWrapper>
-        <Styled.ListItem>
-          <p>
-            <strong>Retrofit</strong>
-          </p>
-          <Styled.Link
-            href="https://github.com/square/retrofit"
-            target="_blank"
-          >
-            https://github.com/square/retrofit
-          </Styled.Link>
-          <p>Copyright 2013 Square, Inc.</p>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>Gson</strong>
-          </p>
-          <Styled.Link href="https://github.com/google/gson" target="_blank">
-            https://github.com/google/gson
-          </Styled.Link>
-          <p>Copyright 2008 Google Inc.</p>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>RxJava</strong>
-          </p>
-          <Styled.Link
-            href="https://github.com/ReactiveX/RxJava"
-            target="_blank"
-          >
-            https://github.com/ReactiveX/RxJava
-          </Styled.Link>
-          <p>Copyright (c) 2016-present, RxJava Contributors</p>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>EventBus</strong>
-          </p>
-          <Styled.Link
-            href="https://github.com/greenrobot/EventBus"
-            target="_blank"
-          >
-            https://github.com/greenrobot/EventBus
-          </Styled.Link>
-          <p>
-            Copyright (C) 2012-2021 Markus Junginger, greenrobot (
-            <Styled.Link href="https://greenrobot.org" target="_blank">
-              https://greenrobot.org
-            </Styled.Link>
-            )
-          </p>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>joda-time-android</strong>
-          </p>
-          <Styled.Link
-            href="https://github.com/dlew/joda-time-android?tab=Apache-2.0-1-ov-file"
-            target="_blank"
-          >
-            https://github.com/dlew/joda-time-android?tab=Apache-2.0-1-ov-file
-          </Styled.Link>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>timber</strong>
-          </p>
-          <Styled.Link
-            href="https://github.com/JakeWharton/timber"
-            target="_blank"
-          >
-            https://github.com/JakeWharton/timber
-          </Styled.Link>
-          <p>Copyright 2013 Jake Wharton.</p>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>DynamicGrid</strong>
-          </p>
-          <Styled.Link
-            href="https://github.com/askerov/DynamicGrid"
-            target="_blank"
-          >
-            https://github.com/askerov/DynamicGrid
-          </Styled.Link>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>Datetimepicker</strong>
-          </p>
-          <Styled.Link
-            href="https://android.googlesource.com/platform/frameworks/opt/datetimepicker"
-            target="_blank"
-          >
-            https://android.googlesource.com/platform/frameworks/opt/datetimepicker
-          </Styled.Link>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>Volley</strong>
-          </p>
-          <Styled.Link href="https://google.github.io/volley/" target="_blank">
-            https://google.github.io/volley/
-          </Styled.Link>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>Apache POI Common</strong>
-          </p>
-          <Styled.Link href="https://github.com/apache/poi" target="_blank">
-            https://github.com/apache/poi
-          </Styled.Link>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>ExpandableLayout</strong>
-          </p>
-          <Styled.Link
-            href="https://github.com/skydoves/ExpandableLayout"
-            target="_blank"
-          >
-            https://github.com/skydoves/ExpandableLayout
-          </Styled.Link>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
-        <Styled.ListItem>
-          <p>
-            <strong>Apache Commons</strong>
-          </p>
-          <Styled.Link href="https://commons.apache.org/" target="_blank">
-            https://commons.apache.org/
-          </Styled.Link>
-          <p>Apache License 2.0</p>
-        </Styled.ListItem>
+        <OssItem
+          label="Retrofit"
+          link="https://github.com/square/retrofit"
+          descriptions={['Copyright 2013 Square, Inc.', 'Apache License 2.0']}
+        />
+        <OssItem
+          label="Gson"
+          link="https://github.com/google/gson"
+          descriptions={['Copyright 2008 Google Inc.', 'Apache License 2.0']}
+        />
+        <OssItem
+          label="RxJava"
+          link="https://github.com/ReactiveX/RxJava"
+          descriptions={[
+            'Copyright (c) 2016-present, RxJava Contributors',
+            'Apache License 2.0',
+          ]}
+        />
+        <OssItem
+          label="EventBus"
+          link="https://github.com/greenrobot/EventBus"
+          subLink="https://greenrobot.org"
+          descriptions={[
+            'Copyright (C) 2012-2021 Markus Junginger, greenrobot',
+            'Apache License 2.0',
+          ]}
+        />
+        <OssItem
+          label="joda-time-android"
+          link="https://github.com/dlew/joda-time-android?tab=Apache-2.0-1-ov-file"
+          descriptions={['Apache License 2.0']}
+        />
+        <OssItem
+          label="timber"
+          link="https://github.com/JakeWharton/timber"
+          descriptions={['Copyright 2013 Jake Wharton.', 'Apache License 2.0']}
+        />
+        <OssItem
+          label="DynamicGrid"
+          link="https://github.com/askerov/DynamicGrid"
+          descriptions={['Apache License 2.0']}
+        />
+        <OssItem
+          label="Datetimepicker"
+          link="https://android.googlesource.com/platform/frameworks/opt/datetimepicker"
+          descriptions={['Apache License 2.0']}
+        />
+        <OssItem
+          label="Volley"
+          link="https://google.github.io/volley"
+          descriptions={['Apache License 2.0']}
+        />
+        <OssItem
+          label="Apache POI Common"
+          link="https://github.com/apache/poi"
+          descriptions={['Apache License 2.0']}
+        />
+        <OssItem
+          label="ExpandableLayout"
+          link="https://github.com/skydoves/ExpandableLayout"
+          descriptions={['Apache License 2.0']}
+        />
+        <OssItem
+          label="Apache Commons"
+          link="https://commons.apache.org/"
+          descriptions={['Apache License 2.0']}
+        />
       </Styled.ListWrapper>
     </Styled.Container>
   );
